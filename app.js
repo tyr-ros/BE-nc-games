b2c48349f184ec938a430330ea1028f94835bda1
Tyr <77638286+tyr-ros@users.noreply.github.com>
2023-05-10 12:20:10 +0100

    4-get-api-reviews

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const { getAllCategories } = require('./controllers/categories-controllers')
 const { getEndpoints } = require('./controllers/api.controller')
+const { getReviewById } = require('./controllers/reviews.controller')
 const app = express()
 
 app.get('/api', getEndpoints)
 
 app.get('/api/categories', getAllCategories)
+
+app.get('/api/reviews/:review_id', getReviewById)
 
 app.use((err, req, res, next) => {
     res.status(err.status).send({ msg: 'Bad request' })
